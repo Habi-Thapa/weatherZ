c846b0f38a17a49f04a125976503ce95b41a91e6
Rubi Thapa <habi.thapa@gmail.com>
2024-05-13 01:41:01 +0545

    Form to search zip code added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useWeatherByZip } from "./api/hooks/useWeatherByZip";
+import { useState } from "react";
 
 const App = () => {
-  const { loading, error, data } = useWeatherByZip("1");
-
+  const [zipCode, setZipCode] = useState("");
+  const { loading, error, data } = useWeatherByZip(zipCode);
   return (
+    // <>
+    //   {loading && <p>Loading...</p>}
+    //   {error && <p>{error}</p>}
+    //   {!loading && data && <p>{JSON.stringify(data)}</p>}
+    // </>
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {!loading && data && <p>{JSON.stringify(data)}</p>}
+      <CssBaseline />
+      <AppBar component="nav" position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            WeatherZ
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Grid container>
+        {/* <Grid item xs={12}>
+          Hero Section
+        </Grid> */}
+        <Grid item xs={12} className="bg-gray-200 py-8">
+          <Container maxWidth="md" style={{ display: "flex" }}>
+            <TextField
+              fullWidth
+              sx={{ borderRadius: 0, backgroundColor: "white" }}
+              label="Enter Zip Code"
+              id="zipCode"
+              variant="outlined"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{
+                fontWeight: "bold",
+                borderRadius: "0 4px 4px 0",
+                ml: -1,
+                px: 3,
+                width: "auto",
+                whiteSpace: "nowrap",
+              }}
+            >
+              Get Weather Info
+            </Button>
+          </Container>
+        </Grid>
+        <Grid item xs={12}>
+          <Container maxWidth="md" sx={{ py: 4 }}>
+            Weather Data
+            {!loading && data && <p>{JSON.stringify(data)}</p>}
+          </Container>
+        </Grid>
+      </Grid>
     </>
   );
 };
